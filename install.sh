ed63722284351be345e739e4c9884fd40d5c8e45
hadinasiri <hadinasiri.6970@gmail.com>
2024-05-25 20:25:50 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -174,17 +174,7 @@ echo "<VirtualHost *:80>
 
 # vim: syntax=apache ts=4 sw=4 sts=4 sr noet" > /etc/apache2/sites-available/000-default.conf
 wait
-##Replace 'Virtual Hosts' and 'List' entries with the new port number
-echo "Listen 80
-<IfModule ssl_module>
-    Listen 80
-</IfModule>
 
-<IfModule mod_gnutls.c>
-    Listen 80
-</IfModule>" > /etc/apache2/ports.conf
-
-wait
 ##Restart the apache server to use new port
 sudo /etc/init.d/apache2 reload
 sudo service apache2 restart
